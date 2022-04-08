c90ec7ac15f31f10bcebeb6e5dde28dc12f127c0
Timmy Jose <zoltan.jose@gmail.com>
2022-04-08 19:17:03 +0530

    Added jline2 support for a saner REPL experience

--- src/main/java/com/tzj/rdlisp/App.java
+++ src/main/java/com/tzj/rdlisp/App.java
@@ -1,21 +1,22 @@
 package com.tzj.rdlisp;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import jline.console.ConsoleReader;
 
 public class App {
   private static final String PROMPT = "> ";
 
   public static void main(String[] args) {
-    try (var in = new BufferedReader(new InputStreamReader(System.in))) {
+    try (ConsoleReader console = new ConsoleReader()) {
       Reader reader;
+      var out = new PrintWriter(console.getOutput());
 
       while (true) {
         try {
-          System.out.print(PROMPT);
-          reader = new Reader(new Lexer(in.readLine().trim()));
+          console.setPrompt(App.PROMPT);
+          reader = new Reader(new Lexer(console.readLine().trim()));
           for (var expr : reader.read()) {
-            System.out.println(Evaluator.eval(Environment.getInitEnv(), expr));
+            out.println(Evaluator.eval(Environment.getInitEnv(), expr));
           }
         } catch (Error err) {
           System.out.println(err);
